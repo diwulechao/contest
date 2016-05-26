0e3e8c59fbb126bb379ae0cf86a67ebaf66ab380
diwulechao <diwulechao@gmail.com>
2016-05-25 18:21:09 -0700

    6

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -23,11 +23,19 @@ namespace WebApplication1.Controllers
         {
             using (var client = new HttpClient())
             {
-                var ret = await client.GetStringAsync("https://graph.facebook.com/v2.3/oauth/access_token?client_id=619387381484849&redirect_uri=http://webapplication16742.azurewebsites.net/home/about&client_secret=1f22ecd5cfa27759fbf126531994531c&code=" + code);
-                TokenClass token = JsonConvert.DeserializeObject<TokenClass>(ret);
+                var s1 = await client.GetStringAsync("https://graph.facebook.com/v2.3/oauth/access_token?client_id=619387381484849&redirect_uri=http://webapplication16742.azurewebsites.net/home/about&client_secret=1f22ecd5cfa27759fbf126531994531c&code=" + code);
+                TokenClass token = JsonConvert.DeserializeObject<TokenClass>(s1);
                 accesstoken = token.access_token;
-                //var tp = await client.GetStringAsync("https://graph.facebook.com/debug_token?input_token=" + accesstoken + "&access_token=ac1cda474284a799511c2edd3f76bce7");
-                ViewBag.Message = accesstoken;
+                var tp = await client.GetStringAsync("https://graph.facebook.com/v2.6/me/posts?access_token="+accesstoken);
+                PostData data = JsonConvert.DeserializeObject<PostData>(tp);
+                var ret = "";
+                if (data.data.Count >= 1)
+                {
+                    ret = data.data[0].message;
+                }
+                else ret = accesstoken;
+
+                ViewBag.Message = ret;
             }
 
             return View();
---
+++ WebApplication1/Models/PostData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class PostData
+    {
+        public List<SingleData> data;
+        public PagingData paging;
+    }
+
+    public class SingleData
+    {
+        public string message;
+        public string created_time;
+        public string id;
+    }
+    public class PagingData
+    {
+        public string previous;
+        public string next;
+    }
+}
