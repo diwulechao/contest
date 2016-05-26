c199d3eaef4df812d05a4b8dc1a150925b8d6fd8
diwulechao <diwulechao@gmail.com>
2016-05-25 17:43:36 -0700

    3

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace WebApplication1.Controllers
 {
@@ -13,16 +15,13 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public async Task<ActionResult> About(string code)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+            using (var client = new HttpClient())
+            {
+                var ret = await client.GetStringAsync("https://graph.facebook.com/v2.3/oauth/access_token?client_id=619387381484849&redirect_uri=http://webapplication16742.azurewebsites.net/home/about&client_secret=1f22ecd5cfa27759fbf126531994531c&code=" + code);
+                ViewBag.Message = ret;
+            }
 
             return View();
         }
---
+++ WebApplication1/Controllers/afterloginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class afterloginController : ApiController
+    {
+        public IHttpActionResult GetProduct(string code)
+        {
+            return Ok(code);
+        }
+
+    }
+}
