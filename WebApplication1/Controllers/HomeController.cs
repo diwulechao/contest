d1efde38d8c138dc34060bd40b7eaa8716265ca5
diwulechao <diwulechao@gmail.com>
2016-05-25 17:57:53 -0700

    4

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -5,11 +5,15 @@ using System.Web;
 using System.Web.Mvc;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        public static string accesstoken;
+
         public ActionResult Index()
         {
             return View();
@@ -20,7 +24,10 @@ namespace WebApplication1.Controllers
             using (var client = new HttpClient())
             {
                 var ret = await client.GetStringAsync("https://graph.facebook.com/v2.3/oauth/access_token?client_id=619387381484849&redirect_uri=http://webapplication16742.azurewebsites.net/home/about&client_secret=1f22ecd5cfa27759fbf126531994531c&code=" + code);
-                ViewBag.Message = ret;
+                TokenClass token = JsonConvert.DeserializeObject<TokenClass>(ret);
+                accesstoken = token.access_token;
+                var tp = await client.GetStringAsync("https://graph.facebook.com/debug_token?input_token=" + accesstoken + "&access_token=ac1cda474284a799511c2edd3f76bce7");
+                ViewBag.Message = tp;
             }
 
             return View();
---
+++ WebApplication1/Models/TokenClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class TokenClass
+    {
+        public string access_token;
+        public string token_type;
+        public string expires_in;
+    }
+}
